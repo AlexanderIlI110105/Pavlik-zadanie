df0acb71bcc1b360b45563732e542248b733259a
AlexanderIlI110105 <143783059+AlexanderIlI110105@users.noreply.github.com>
2023-09-05 10:40:57 +0300

    Add files via upload

---
+++ Zadanie #1.java
@@ -0,0 +1,8 @@
+public class ${NAME} {
+    public static void main(String[] args) {
+        ThreeWords.printThreeWords();
+        CheckSumSign.checkSumSign(3, -5);
+        Color.printColor(1000);
+        Numbers.compeNumbers(30, 5);
+    }
+}
